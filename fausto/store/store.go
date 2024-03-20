6e42e8a85ddd6a3dd93e6adeef8ec58bdfa4e286
gustrb <gustavoreisbauer@gmail.com>
2024-03-19 21:35:19 -0300

    feat: Add database connection
    Also added a dockerfile+compose

---
+++ fausto/store/store.go
@@ -0,0 +1,65 @@
+package store
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var _store DataStore
+var _client *mongo.Client
+
+const (
+	ClientDefaultTimeout = 30 * time.Second
+)
+
+type DataStore interface {
+	FileStore() *FileStore
+}
+
+type CreateFileDTO struct {
+	Id      primitive.ObjectID `json:"id" bson:"_id"`
+	Content string             `json:"content" bson:"content"`
+	Name    string             `json:"name" bson:"name"`
+}
+
+type FileStore interface {
+	createFile(*CreateFileDTO) error
+}
+
+func Initialize(uri string) error {
+	ctx := context.Background()
+
+	clientOpts := options.Client()
+	clientOpts.ApplyURI(uri)
+	clientOpts.SetConnectTimeout(ClientDefaultTimeout)
+	client, err := mongo.Connect(ctx, clientOpts)
+
+	if err != nil {
+		return fmt.Errorf("mongodrv: could not connect: %v", err)
+	}
+
+	if err = client.Ping(ctx, nil); err != nil {
+		return err
+	}
+
+	_client = client
+	return nil
+}
+
+func Disconnect() error {
+	if _client == nil {
+		return errors.New("cannot disconnect unconnected server")
+	}
+
+	return _client.Disconnect(context.TODO())
+}
+
+func GetStore() DataStore {
+	return _store
+}
