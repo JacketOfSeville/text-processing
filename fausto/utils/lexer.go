81b2287b8aa14afabd0792e9b17b4a7bcb69951d
gustrb <gustavoreisbauer@gmail.com>
2024-04-27 11:44:18 -0300

    feat: Add line and column info to the lexer

--- fausto/utils/lexer.go
+++ fausto/utils/lexer.go
@@ -1,6 +1,8 @@
 package utils
 
-import "unicode"
+import (
+	"unicode"
+)
 
 const (
 	TokWord         = iota
@@ -9,27 +11,49 @@ const (
 )
 
 type Token struct {
-	Type  int
-	Value string
+	Type   int
+	Value  string
+	Line   int
+	Column int
 }
 
 type Lexer struct {
 	text string
 	pos  int
+	line int
+	col  int
 }
 
 func NewLexer(text string) *Lexer {
-	return &Lexer{text: text, pos: 0}
+	return &Lexer{text: text, pos: 0, line: 0, col: 0}
+}
+
+func (l *Lexer) advance() {
+	if l.pos >= len(l.text) {
+		return
+	}
+
+	if l.text[l.pos] == '\n' {
+		l.line++
+		l.col = 0
+	}
+	if l.text[l.pos] == '\t' {
+		l.col += 4
+	} else {
+		l.col++
+	}
+
+	l.pos++
 }
 
 func (l *Lexer) NextToken() Token {
 	// Skip whitespaces
 	for l.pos < len(l.text) && unicode.IsSpace(rune(l.text[l.pos])) {
-		l.pos++
+		l.advance()
 	}
 
 	if l.pos >= len(l.text) {
-		return Token{Type: TokEOF, Value: ""}
+		return Token{Type: TokEOF, Value: "", Line: l.line, Column: l.col}
 	}
 
 	r := rune(l.text[l.pos])
@@ -39,14 +63,14 @@ func (l *Lexer) NextToken() Token {
 
 	l.pos++
 
-	return Token{Type: TokPunctuations, Value: string(r)}
+	return Token{Type: TokPunctuations, Value: string(r), Line: l.line, Column: l.col}
 }
 
 func (l *Lexer) consumeWord() Token {
 	start := l.pos
 	for l.pos < len(l.text) && unicode.IsLetter(rune(l.text[l.pos])) {
-		l.pos++
+		l.advance()
 	}
 
-	return Token{Type: TokWord, Value: l.text[start:l.pos]}
+	return Token{Type: TokWord, Value: l.text[start:l.pos], Line: l.line, Column: l.col}
 }
